81cb9126582580313ebebf3ed51960729577a986
Tom Counsell <other@tomcounsell.com>
2018-09-22 20:50:27 +0100

    remove unused logic

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -15,17 +15,6 @@ import Button from '@material-ui/core/Button';
 import Login from './login';
 import Results from './results';
 
-// NEVER store private keys in any source code in your real life development
-// This is for demo purposes only!
-const accounts = [
-  {"name":"useraaaaaaaa", "privateKey":"5K7mtrinTFrVTduSxizUc5hjXJEtTjVTsqSHeBHes1Viep86FP5", "publicKey":"EOS6kYgMTCh1iqpq9XGNQbEi8Q6k5GujefN9DSs55dcjVyFAq7B6b"},
-  {"name":"useraaaaaaab", "privateKey":"5KLqT1UFxVnKRWkjvhFur4sECrPhciuUqsYRihc1p9rxhXQMZBg", "publicKey":"EOS78RuuHNgtmDv9jwAzhxZ9LmC6F295snyQ9eUDQ5YtVHJ1udE6p"},
-  {"name":"useraaaaaaac", "privateKey":"5K2jun7wohStgiCDSDYjk3eteRH1KaxUQsZTEmTGPH4GS9vVFb7", "publicKey":"EOS5yd9aufDv7MqMquGcQdD6Bfmv6umqSuh9ru3kheDBqbi6vtJ58"},
-  {"name":"useraaaaaaad", "privateKey":"5KNm1BgaopP9n5NqJDo9rbr49zJFWJTMJheLoLM5b7gjdhqAwCx", "publicKey":"EOS8LoJJUU3dhiFyJ5HmsMiAuNLGc6HMkxF4Etx6pxLRG7FU89x6X"},
-  {"name":"useraaaaaaae", "privateKey":"5KE2UNPCZX5QepKcLpLXVCLdAw7dBfJFJnuCHhXUf61hPRMtUZg", "publicKey":"EOS7XPiPuL3jbgpfS3FFmjtXK62Th9n2WZdvJb6XLygAghfx1W7Nb"},
-  {"name":"useraaaaaaaf", "privateKey":"5KaqYiQzKsXXXxVvrG8Q3ECZdQAj2hNcvCgGEubRvvq7CU3LySK", "publicKey":"EOS5btzHW33f9zbhkwjJTYsoyRzXUNstx1Da9X2nTzk8BQztxoP3H"},
-  {"name":"useraaaaaaag", "privateKey":"5KFyaxQW8L6uXFB6wSgC44EsAbzC7ideyhhQ68tiYfdKQp69xKo", "publicKey":"EOS8Du668rSVDE3KkmhwKkmAyxdBd73B51FKE7SjkKe5YERBULMrw"}
-];
 
 // set up styling classes using material-ui "withStyles"
 const styles = theme => ({
@@ -36,10 +25,6 @@ const styles = theme => ({
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing.unit * 2,
     paddingBottom: theme.spacing.unit * 2,
-  },
-  formButton: {
-    marginTop: theme.spacing.unit,
-    width: "100%",
   },
   pre: {
     background: "#ccc",
@@ -60,88 +45,9 @@ class Index extends Component {
     this.handleFormEvent = this.handleFormEvent.bind(this);
   }
 
-  // generic function to handle form events (e.g. "submit" / "reset")
-  // push transactions to the blockchain by using eosjs
-  async handleFormEvent(event) {
-    // stop default behaviour
-    event.preventDefault();
-
-    // collect form data
-    let account = event.target.account.value;
-    let privateKey = event.target.privateKey.value;
-    let showLogin = true;
-
-    // prepare variables for the switch below to send transactions
-    let actionName = "";
-    let actionData = {};
-
-    // define actionName and action according to event type
-    switch (event.type) {
-      case "submit":
-        actionName = "update";
-        break;
-      default:
-        return;
-    }
-
-    // eosjs function call: connect to the blockchain
-    const eos = Eos({keyProvider: privateKey});
-    const result = await eos.transaction({
-      actions: [{
-        account: "notechainacc",
-        name: actionName,
-        authorization: [{
-          actor: account,
-          permission: 'active',
-        }],
-        data: actionData,
-      }],
-    });
-
-    console.log(result);
-    this.getTable();
-  }
-
-  // gets table data from the blockchain
-  // and saves it into the component state: "noteTable"
-  getTable() {
-    const eos = Eos();
-    eos.getTableRows({
-      "json": true,
-      "code": "notechainacc",   // contract who owns the table
-      "scope": "notechainacc",  // scope of the table
-      "table": "notestruct",    // name of the table as specified by the contract abi
-      "limit": 100,
-    }).then(result => this.setState({ noteTable: result.rows }));
-  }
-
-  componentDidMount() {
-    this.getTable();
-  }
 
   render() {
-    const { noteTable } = this.state;
     const { classes } = this.props;
-
-    // generate each note as a card
-    const generateCard = (key, timestamp, user, note) => (
-      <Card className={classes.card} key={key}>
-        <CardContent>
-          <Typography variant="headline" component="h2">
-            {user}
-          </Typography>
-          <Typography style={{fontSize:12}} color="textSecondary" gutterBottom>
-            {new Date(timestamp*1000).toString()}
-          </Typography>
-          <Typography component="pre">
-            {note}
-          </Typography>
-        </CardContent>
-      </Card>
-    );
-    let noteCards = noteTable.map((row, i) =>
-      generateCard(i, row.timestamp, row.user, row.note));
-
 
     return (
       <div>
@@ -157,7 +63,6 @@ class Index extends Component {
           { this.state.showLogin ? <Login /> : <Results /> }
 
         </Paper>
-
       </div>
     );
   }
